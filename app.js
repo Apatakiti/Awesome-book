1d34b3bfbc7950fc8b301c134c44743a49b38c8c
ademola <ademolaowoeye07@gmail.com>
2022-08-03 20:06:26 +0100

    fix linters

--- app.js
+++ app.js
@@ -1,50 +1,45 @@
-      const booksContainer = document.getElementById('books')
-      const inpTitle = document.getElementById('inp-title')
-      const inpAuthor = document.getElementById('inp-author')
+const booksContainer = document.getElementById('books');
+const inpTitle = document.getElementById('inp-title');
+const inpAuthor = document.getElementById('inp-author');
 
-      const bookDatas = [];
+const bookDatas = [];
 
-      class Book {
-        constructor(title, author) {
-          this.title = title;
-          this.author = author;
-        }
-      }
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+}
 
-       function generateBook() {
-        booksContainer.innerHTML = '';
-      for (let i = 0; i < bookDatas.length; i+=1  ) {
-        booksContainer.innerHTML += `<div class="book">
+function generateBook() {
+  booksContainer.innerHTML = '';
+  for (let i = 0; i < bookDatas.length; i += 1) {
+    booksContainer.innerHTML += `<div class="book">
                      <div>${bookDatas[i].title}</div>
                      <div>${bookDatas[i].author}</div>
                      <button class="rem-btn" onclick="removeBook(${i})">Remove</button>
-                     <hr><br></div>`}
-    }
-    
-    function removeBook(i) {
-      bookDatas.splice(i, 1);
-      generateBook();
-    }
+                     <hr><br></div>`;
+  }
+}
 
-    function generateNewBook(i) {
-        booksContainer.innerHTML += `<div class="book" id="book">
+function removeBook(i) {
+  bookDatas.splice(i, 1);
+  generateBook();
+}
+
+function generateNewBook(i) {
+  booksContainer.innerHTML += `<div class="book" id="book">
                      <div class="the-title">${bookDatas[i].title}</div>
                      <div class="the-author">${bookDatas[i].author}</div>
                      <button class="rem-btn" onclick="removeBook(${i})">Remove</button>
-                     <hr><br></div>`
-    }
+                     <hr><br></div>`;
+}
 
-        const addBtn = document.getElementById('add-btn')
-        addBtn.addEventListener('click', () => {
-        const newBook = new Book(inpTitle.value, inpAuthor.value);
-        bookDatas.push(newBook);
-        generateNewBook(bookDatas.length - 1);
-        localStorage.setItem('bookDatas', JSON.stringify(bookDatas));
-       });
-     removeBook();
-
-
-
-
-
-
+const addBtn = document.getElementById('add-btn');
+addBtn.addEventListener('click', () => {
+  const newBook = new Book(inpTitle.value, inpAuthor.value);
+  bookDatas.push(newBook);
+  generateNewBook(bookDatas.length - 1);
+  localStorage.setItem('bookDatas', JSON.stringify(bookDatas));
+});
+removeBook();

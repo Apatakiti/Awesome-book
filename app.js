309a90826765eafa206ebdc5cf0096d44b32cf80
ademola <ademolaowoeye07@gmail.com>
2022-08-04 17:09:35 +0100

    using classes to add

--- app.js
+++ app.js
@@ -3,19 +3,27 @@ const inpTitle = document.getElementById('inp-title');
 const inpAuthor = document.getElementById('inp-author');
 
 class Book {
-
  static bookDatas = []
 
-  constructor(title, author) {
-    this.title = title;
-    this.author = author;
-  }
+ constructor(title, author) {
+   this.title = title;
+   this.author = author;
+ }
+
+ //adding
+ static addBook(Dbook) {
+  this.bookDatas.push(Dbook);
+ }
+
+ //removing
+ static remBook(i) {
+      this.bookDatas.splice(i, 1);
+    }
 }
 
 function generateBook() {
   booksContainer.innerHTML = '';
   for (let i = 0; i < Book.bookDatas.length; i += 1) {
-            
     booksContainer.innerHTML += `<div class="book">
                      <div class="titleAndBooks">"${Book.bookDatas[i].title}" by ${Book.bookDatas[i].author}</div> <button class="rem-btn" onclick="removeBook(${i})">Remove</button>
                      </div>
@@ -23,8 +31,9 @@ function generateBook() {
   }
 }
 
+//remove book
 function removeBook(i) {
-  Book.bookDatas.splice(i, 1);
+  Book.remBook(i);
   generateBook();
 }
 
@@ -34,12 +43,11 @@ function generateNewBook(i) {
   </div>`;
 }
 
+//adding book
 const addBtn = document.getElementById('add-btn');
-
 addBtn.addEventListener('click', () => {
   const newBook = new Book(inpTitle.value, inpAuthor.value);
-  Book.bookDatas.push(newBook);
-
+  Book.addBook(newBook);
   generateNewBook(Book.bookDatas.length - 1);
 
   localStorage.setItem('bookD', JSON.stringify(Book.bookDatas));
@@ -47,7 +55,7 @@ addBtn.addEventListener('click', () => {
 removeBook();
 
 window.addEventListener('load', () => {
-  Book.bookDatas = JSON.parse(localStorage.getItem('bookD'));
+  Book.bookDatas = JSON.parse(localStorage.getItem('bookD')); 
   if (Book.bookDatas) {
     generateBook();
   } else {

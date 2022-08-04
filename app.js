509c141525b7f906e2209a9d09668afd54bfe37c
ademola <ademolaowoeye07@gmail.com>
2022-08-04 15:14:19 +0100

    reFactoring

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const booksContainer = document.getElementById('books');
 const inpTitle = document.getElementById('inp-title');
 const inpAuthor = document.getElementById('inp-author');
 
-const bookDatas = [];
-
 class Book {
+
+ static bookDatas = []
+
   constructor(title, author) {
     this.title = title;
     this.author = author;
@@ -13,42 +14,43 @@ class Book {
 
 function generateBook() {
   booksContainer.innerHTML = '';
-  for (let i = 0; i < bookDatas.length; i += 1) {
+  for (let i = 0; i < Book.bookDatas.length; i += 1) {
+            
     booksContainer.innerHTML += `<div class="book">
-                     <div>${bookDatas[i].title}</div>
-                     <div>${bookDatas[i].author}</div>
-                     <button class="rem-btn" onclick="removeBook(${i})">Remove</button>
-                     <hr><br></div>`;
+                     <div class="titleAndBooks">"${Book.bookDatas[i].title}" by ${Book.bookDatas[i].author}</div> <button class="rem-btn" onclick="removeBook(${i})">Remove</button>
+                     </div>
+                     `;
   }
 }
 
 function removeBook(i) {
-  bookDatas.splice(i, 1);
+  Book.bookDatas.splice(i, 1);
   generateBook();
 }
 
 function generateNewBook(i) {
-  booksContainer.innerHTML += `<div class="book" id="book">
-                     <div class="the-title">${bookDatas[i].title}</div>
-                     <div class="the-author">${bookDatas[i].author}</div>
-                     <button class="rem-btn" onclick="removeBook(${i})">Remove</button>
-                     <hr><br></div>`;
+  booksContainer.innerHTML += `<div class="book">
+  <div class="titleAndBooks">"${Book.bookDatas[i].title}" by ${Book.bookDatas[i].author}</div> <button class="rem-btn" onclick="removeBook(${i})">Remove</button>
+  </div>`;
 }
 
 const addBtn = document.getElementById('add-btn');
+
 addBtn.addEventListener('click', () => {
   const newBook = new Book(inpTitle.value, inpAuthor.value);
-  bookDatas.push(newBook);
-  generateNewBook(bookDatas.length - 1);
-  localStorage.setItem('bookDatas', JSON.stringify(bookDatas));
+  Book.bookDatas.push(newBook);
+
+  generateNewBook(Book.bookDatas.length - 1);
+
+  localStorage.setItem('bookD', JSON.stringify(Book.bookDatas));
 });
 removeBook();
 
 window.addEventListener('load', () => {
-  let bookData = JSON.parse(localStorage.getItem('bookDatas'));
-  if (bookData) {
+  Book.bookDatas = JSON.parse(localStorage.getItem('bookD'));
+  if (Book.bookDatas) {
     generateBook();
   } else {
-    bookData = [];
+    Book.bookDatas = [];
   }
 });
